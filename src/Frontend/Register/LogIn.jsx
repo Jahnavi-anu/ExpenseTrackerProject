d702582c1f2f843ad827aa890f22e04ad385d5db
Jahnavi-anu <jahnaviveeranala80@gmail.com>
2025-11-06 11:12:09 +0530

    changes

--- src/Frontend/Register/LogIn.jsx
+++ src/Frontend/Register/LogIn.jsx
@@ -1,5 +1,5 @@
 
-import { Link,Navigate, useNavigate } from 'react-router-dom';
+import { Link,useNavigate } from 'react-router-dom';
 import './Login.css'
 import axios from 'axios';
 import {AUTH_API} from '../../config.js'
